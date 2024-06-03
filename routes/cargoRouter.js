44be8fbc3e0867384de115a5b31b256bae860d44
Test-demo <zoyakh1686@gmail.com>
2024-06-03 13:29:27 +0530

    Add a new project

---
+++ routes/cargoRouter.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const router = express.Router();
+const Cargo = require('../model/cargoModel');
+
+router.get('/', async (req, res) => {
+    //res.send('Get Req');
+    try {
+        const cargos = await Cargo.find();
+        res.json(cargos);
+    } catch (err) {
+        res.send('Error:' + err);
+    }
+});
+
+router.get('/:_id', async (req, res) => {
+    try {
+        const cargoId = await Cargo.findById(req.params._id);
+        res.json(cargoId);
+    }
+    catch (err) {
+        res.send('Error:' + err);
+    }
+});
+
+router.post('/cargoDetails', async (req, res) => {
+    const cargoForm = new Cargo({
+        senderCompany:req.body.senderCompany,
+        receiverCompany:req.body.receiverCompany,
+        invoiceNo: req.body.invoiceNo,
+        Date: req.body.Date,
+        From: req.body.From,
+        To: req.body.To,
+        Email:req.body.Email,
+        trainNo:req.body.trainNo,
+        noOfArticles: req.body.noOfArticles,
+        packingMode: req.body.packingMode,
+        Description: req.body.Description,
+        actualWeight: req.body.actualWeight,
+        doubleWeight: req.body.doubleWeight,
+        chargeWeight: req.body.chargeWeight,
+        ewayBillNo: req.body.ewayBillNo,
+        Rate: req.body.Rate,
+        Freight: req.body.Freight,
+        Handling: req.body.Handling,
+        gatePass: req.body.gatePass,
+        doorDelivery: req.body.doorDelivery,
+        CGST: req.body.CGST,
+        SGST: req.body.SGST,
+        IGST: req.body.IGST,
+        grandTotal: req.body.grandTotal,
+        Value: req.body.Value,
+        paymentMode: req.body.paymentMode,
+        cargoType:req.body.cargoType,
+        Status:req.body.Status
+    });
+    try {
+        cargoForm.save(cargoForm)
+            .then(cargo => {
+                res.send(cargo);
+            })
+    }
+    catch (err) {
+        res.send('Error:' + err);
+    }
+});
+
+router.put('/', (req, res) => {
+    Cargo.findOneAndUpdate(
+        { _id: req.body._id },
+        { $set: req.body }
+    ).then(datas => {
+        if (datas) {
+            res.status(200).json(datas)
+        }
+        else {
+            res.status(401).json({ error: 'Cargo not exists' })
+        }
+    }).catch(err => {
+        res.status(500).json({ error: err.message })
+    })
+});
+
+router.delete('/:_id', (req, res) => {
+    Cargo.findByIdAndRemove(req.params._id)
+        .then(res => {
+            res.json({ msg: 'Cargo Deleted' })
+        })
+        .catch(err => {
+            res.json(err)
+        })
+});
+
+
+module.exports = router;
---
+++ routes/companyRouter.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const router = express.Router();
+const Company = require('../model/companyModel');
+
+router.get('/', async (req, res) => {
+    //res.send('Get Req');
+    try {
+        const companies = await Company.find();
+        res.json(companies);
+    } catch (err) {
+        res.send('Error:' + err);
+    }
+});
+
+router.get('/:_id', async (req, res) => {
+    try {
+        const companyId = await Company.findById(req.params._id);
+        res.json(companyId);
+    }
+    catch (err) {
+        res.send('Error:' + err);
+    }
+});
+
+router.post('/addCompany', async (req, res) => {
+    if (await companyExists(req.body.companyName)) {
+        res.status(409).json({ error: 'Cargo alreaday exists' })
+    }
+    else {
+        const companyForm = new Company({
+            companyName: req.body.companyName,
+            Email: req.body.Email,
+            GSTNo: req.body.GSTNo,
+            phoneNumber: req.body.phoneNumber,
+            State: req.body.State,
+            stateCode: req.body.stateCode,
+            Address: req.body.Address
+        });
+        try {
+            companyForm.save(companyForm)
+                .then(company => {
+                    res.send(company);
+                })
+        }
+        catch (err) {
+            res.send('Error:' + err);
+        }
+    }
+});
+router.delete('/:_id', (req, res) => {
+    Company.findByIdAndRemove(req.params._id)
+        .then(res => {
+            res.json({ msg: 'Company Deleted' })
+        })
+        .catch(err => {
+            res.json(err)
+        })
+});
+
+router.put('/', (req, res) => {
+    Company.updateOne(
+        { companyName: req.body.companyName },
+        { $set: req.body }
+    ).then(datas => {
+        if (datas) {
+            res.status(200).json(datas)
+        }
+        else {
+            res.status(401).json({ error: 'Company not exists' })
+        }
+    }).catch(err => {
+        res.status(500).json({ error: err.message })
+    })
+
+});
+
+const companyExists = async (companyName) => {
+    const companies = await Company.findOne({ companyName: companyName })
+
+    if (companies) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+module.exports = router;
